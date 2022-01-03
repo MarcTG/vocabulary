80efd208f4342e26663314c88bc4229b8eb739db
MarcTG <marc.tg94@gmail.com>
2022-01-03 14:58:04 -0400

    add tailwind

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="text-xl text-blue-500">Hello world</h1>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
